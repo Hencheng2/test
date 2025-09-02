06d211ad8194b209fa412ccdb2081874b833aae6
Henry Ochieng <111287785+Hencheng2@users.noreply.github.com>
2025-09-02 14:47:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,3 @@
-import os
-
-# Basic configuration
-SECRET_KEY = os.environ.get('SECRET_KEY', '09da35833ef9cb699888f08d66a0cfb827fb10e53f6c1549')
-UPLOAD_FOLDER = 'static/uploads'
-
-# Admin credentials
+SECRET_KEY = '09da35833ef9cb699888f08d66a0cfb827fb10e53f6c1549'
 ADMIN_USERNAME = 'Henry'
 ADMIN_PASS = 'Dec@2003'
-
-# Ensure upload directory exists
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
